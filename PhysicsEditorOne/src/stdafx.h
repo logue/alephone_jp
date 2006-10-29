1164060f24c82d459f9603ad576477ef45dd04d4
hogepiyo <hogepiyo@users.sourceforge.jp>
2006-10-29 05:24:02 +0000

    change OS version

--- PhysicsEditorOne/src/stdafx.h
+++ PhysicsEditorOne/src/stdafx.h
@@ -15,11 +15,11 @@
 // 下で指定された定義の前に対象プラットフォームを指定しなければならない場合、以下の定義を変更してください。
 // 異なるプラットフォームに対応する値に関する最新情報については、MSDN を参照してください。
 #ifndef WINVER				// Windows XP 以降のバージョンに固有の機能の使用を許可します。
-#define WINVER 0x0501		// これを Windows の他のバージョン向けに適切な値に変更してください。
+#define WINVER 0x0410//0x0501		// これを Windows の他のバージョン向けに適切な値に変更してください。
 #endif
 
 #ifndef _WIN32_WINNT		// Windows XP 以降のバージョンに固有の機能の使用を許可します。                   
-#define _WIN32_WINNT 0x0501	// これを Windows の他のバージョン向けに適切な値に変更してください。
+#define _WIN32_WINNT 0x0410	// これを Windows の他のバージョン向けに適切な値に変更してください。
 #endif						
 
 #ifndef _WIN32_WINDOWS		// Windows 98 以降のバージョンに固有の機能の使用を許可します。
