26c96ffd9008508d05c3c2bf0c8a28bceab983e9
hogepiyo <hogepiyo@users.sourceforge.jp>
2007-05-04 09:51:42 +0000

    *** empty log message ***

--- HPLLib/HPLMapTool.cpp
+++ HPLLib/HPLMapTool.cpp
@@ -78,6 +78,8 @@ bool hpl::aleph::map::isSelectLine(world_point2d &point,
         linePoint0.x, linePoint0.y, linePoint1.x, linePoint1.y, distance);
     return isSelect;
 }
+
+
 double hpl::aleph::map::getLineLength(int index)
 {
     line_data* l = get_line_data(index);
@@ -150,10 +152,9 @@ void hpl::aleph::map::fixLine(int index, bool isDeleteOldSide)
 }
 
 ///////////////////////  Sides  ////////////////////////////////////////////
-void hpl::aleph::map::fixSide(int leftPolyIndex, int rightPolyIndex, int sideIndex,
-        int lineIndex, bool isDeleteOldSide)
+void hpl::aleph::map::fixSide(int sideIndex)
 {
-
+    //TODO
 }
 
 ///////////////////////  Groups  ////////////////////////////////////////////
@@ -167,66 +168,75 @@ bool hpl::aleph::map::isPointInSelection(int px, int py,
                         int heightMax, int heightMin, int div)
 {
     //objects
-    std::vector<struct hpl::aleph::map::SelObject>* selobjects = 
-        selectInfo->getSelObjects();
-    for(int i = 0; i < (int)selobjects->size(); i ++){
-        map_object* obj = &(SavedObjectList[selobjects->at(i).index]);
-        int type = obj->type;
-        int facing = obj->facing;
-        int x = obj->location.x;
-        int y = obj->location.y;
-        int z = obj->location.z;
-        if(z > heightMax || z < heightMin){
-            continue;
-        }
-        if(hpl::aleph::map::isSelectPoint(px, py,
-            x, y, offsetViewX, offsetViewY,
-            offsetWorldX, offsetWorldY, div, objectDistance))
-        {
-            return true;
+    {
+        std::vector<struct hpl::aleph::map::SelObject>* selobjects = 
+            selectInfo->getSelObjects();
+        for(int i = 0; i < (int)selobjects->size(); i ++){
+            map_object* obj = &(SavedObjectList[selobjects->at(i).index]);
+            int type = obj->type;
+            int facing = obj->facing;
+            int x = obj->location.x;
+            int y = obj->location.y;
+            int z = obj->location.z;
+            if(z > heightMax || z < heightMin){
+                continue;
+            }
+            if(hpl::aleph::map::isSelectPoint(px, py,
+                x, y, offsetViewX, offsetViewY,
+                offsetWorldX, offsetWorldY, div, objectDistance))
+            {
+                return true;
+            }
         }
     }
+
     //points
-    std::vector<struct hpl::aleph::map::SelPoint>* selpoints = 
-        selectInfo->getSelPoints();
-    for(int i = 0; i < (int)selpoints->size(); i ++){
-        endpoint_data* ep = &EndpointList[selpoints->at(i).index];
-        int x = ep->vertex.x;
-        int y = ep->vertex.y;
-        int drawX = (x + offsetWorldX)/div + offsetViewX;
-        int drawY = (y + offsetWorldY)/div + offsetViewY;
-        if(hpl::aleph::map::isSelectPoint(px, py,
-            x, y, offsetViewX, offsetViewY,
-            offsetWorldX, offsetWorldY, div, objectDistance))
-        {
-            return true;
+    {
+        std::vector<struct hpl::aleph::map::SelPoint>* selpoints = 
+            selectInfo->getSelPoints();
+        for(int i = 0; i < (int)selpoints->size(); i ++){
+            endpoint_data* ep = &EndpointList[selpoints->at(i).index];
+            int x = ep->vertex.x;
+            int y = ep->vertex.y;
+            int drawX = (x + offsetWorldX)/div + offsetViewX;
+            int drawY = (y + offsetWorldY)/div + offsetViewY;
+            if(hpl::aleph::map::isSelectPoint(px, py,
+                x, y, offsetViewX, offsetViewY,
+                offsetWorldX, offsetWorldY, div, objectDistance))
+            {
+                return true;
+            }
         }
     }
 
     //lines
-    std::vector<struct hpl::aleph::map::SelLine>* sellines = 
-        selectInfo->getSelLines();
-    for(int i = 0; i < (int)sellines->size(); i ++){
-        line_data* line = &LineList[sellines->at(i).index];
-        endpoint_data* begin = &EndpointList[line->endpoint_indexes[0]];
-        endpoint_data* end = &EndpointList[line->endpoint_indexes[1]];
-        if(hpl::aleph::map::isSelectLine(px, py, begin->vertex.x, begin->vertex.y,
-            end->vertex.x, end->vertex.y, offsetViewX, offsetViewY,
-            offsetWorldX, offsetWorldY, div, lineDistance))
-        {
-            return true;
+    {
+        std::vector<struct hpl::aleph::map::SelLine>* sellines = 
+            selectInfo->getSelLines();
+        for(int i = 0; i < (int)sellines->size(); i ++){
+            line_data* line = &LineList[sellines->at(i).index];
+            endpoint_data* begin = &EndpointList[line->endpoint_indexes[0]];
+            endpoint_data* end = &EndpointList[line->endpoint_indexes[1]];
+            if(hpl::aleph::map::isSelectLine(px, py, begin->vertex.x, begin->vertex.y,
+                end->vertex.x, end->vertex.y, offsetViewX, offsetViewY,
+                offsetWorldX, offsetWorldY, div, lineDistance))
+            {
+                return true;
+            }
         }
     }
     //polygons
-    std::vector<struct hpl::aleph::map::SelPolygon>* selpolygons = 
-        selectInfo->getSelPolygons();
-    for(int i = 0; i < (int)selpolygons->size(); i ++){
-        struct world_point2d world_point;
-        world_point.x = (world_distance)((px - offsetViewX) * div - offsetWorldX);
-        world_point.y = (world_distance)((py - offsetViewY) * div - offsetWorldY);
+    {
+        std::vector<struct hpl::aleph::map::SelPolygon>* selpolygons = 
+            selectInfo->getSelPolygons();
+        for(int i = 0; i < (int)selpolygons->size(); i ++){
+            struct world_point2d world_point;
+            world_point.x = (world_distance)((px - offsetViewX) * div - offsetWorldX);
+            world_point.y = (world_distance)((py - offsetViewY) * div - offsetWorldY);
 
-        if(point_in_polygon(selobjects->at(i).index, &world_point)){
-            return true;
+            if(point_in_polygon(selpolygons->at(i).index, &world_point)){
+                return true;
+            }
         }
     }
 
--- HPLLib/HPLMapTool.h
+++ HPLLib/HPLMapTool.h
@@ -18,13 +18,13 @@ namespace map{
 	///////////////////////////////////////////////////////////////////////
 	///////////	 Points	 //////////////////////////////////////////////////
 	/**
-		<jp>指定した点をクリックできているかの判定を行います。
+		<jp>指定した点をクリックしたかどうかの判定を行います
 		<en>is view-point near world-point?
-		@param viewPX	point(mouse/view)		ビュー座標（スクリーン座標）
-		@param worldPX	point(world)			対象とする点（実座標）
-		@param offsetViewX offset of view		ビュー座標のズレ
-		@param offsetWorldX offset of world		実座標のズレ
-		@param distance check distance			閾値
+		@param viewPX	point(mouse/view)		    ビュー座標（スクリーン座標）
+		@param worldPX	point(world)			    検査対象とする点（実座標）
+		@param offsetViewX offset of view		    ビュー座標のズレ
+		@param offsetWorldX offset of world		    実座標のズレ
+		@param distance check distance threshold    閾値
 	*/
 	bool isSelectPoint(int viewPX, int viewPY, 
 					   int worldPX, int worldPY,
@@ -32,13 +32,20 @@ namespace map{
 					   int offsetWorldX, int offsetWorldY,
 					   int div,
 					   int distance);
+    /**
+        <jp>指定した点(point1)をマウスの座標(point0)がクリックしたかどうかを判定します
+        <en>check which two world-points are near or not
+        @param point0 world-point0  点
+        @param point1 world-point1  点
+        @param distance threshold   閾値
+    */
 	bool isSelectPoint(world_point2d &point0, world_point2d &point1,
 					   int distance);
 	
 	/**
 		<jp>指定した点をグリッドに合わせ、その座標を得ます
 		<en>snap point to grid
-		@param srcPoint
+		@param srcPoint                         元の座標
 		@param destPoint snapped point			グリッドに合わされた点座標
 		@param gridInterval interval of grids	グリッド間隔
 	*/
@@ -52,19 +59,20 @@ namespace map{
 
 	//convert view <-> world point
 	world_point2d getWorldPoint2DFromViewPoint(int viewPX, int viewPY,
-	int offsetXWorld, int offsetYWorld, int zoomDivision, int offsetx, int offsety);
+	    int offsetXWorld, int offsetYWorld, int zoomDivision, int offsetx, int offsety);
 	void getViewPointFromWorldPoint2D(world_point2d& point, int *dest,
-	int offsetXWorld, int offsetYWorld, int zoomDivision, int offsetx, int offsety);
+	    int offsetXWorld, int offsetYWorld, int zoomDivision, int offsetx, int offsety);
 
 	///////////////////////	 Lines	////////////////////////////////////////////
 	/**
 		指定した点を用いたときに線を選択できているかを判定
 		is view-point near world-line?
 		@param viewPX	point(mouse/view)
-		@param worldPX0 line's point(world)
-		@param offsetViewX offset of view
-		@param offsetWorldX offset of world
-		@param distance check distance
+		@param worldPX0 line's point(world)                     
+		@param offsetViewX offset of view                       view座標のズレ
+		@param offsetWorldX offset of world                     world座標のズレ（中心位置）
+		@param distance distance threshold which grab or not    近いかどうかの閾値
+        @return true when mouse can grab line                   線を選択できていればtrue
 	*/
 	bool isSelectLine(int viewPX, int viewPY,
 					   int worldPX0, int worldPY0,
@@ -74,17 +82,31 @@ namespace map{
 					   int div,
 					   int distance);
 
+    /**
+        <jp>線を選択できているか判断
+        <en>check which mouse grob line
+        @param point mouse location in view
+        @param linePoint0
+        @param linePoint1 line's location in world
+        @param distance distance threshold which grab or not
+        @return true when mouse can grab line
+    */
 	bool isSelectLine(world_point2d &point,
 					  world_point2d &linePoint0, world_point2d &linePoint1,
 					  int distance);
 
 	/**
 		線の長さを取得
+        get line's length (calc length, not get from its structure::length)
 		@param index 線データのインデックス
+        @return line(index)'s length
 	*/
 	double getLineLength(int index);
+
 	/*
 		線の長さを得る
+        get line's length from two world points
+        
 	*/
 	double getPointsDistance(world_point2d& pointA, world_point2d& pointB);
 
@@ -98,6 +120,10 @@ namespace map{
 	////////////////////////////////////////////////////////////////////////
 	///////////	 Sides	////////////////////////////////////////////////////
 
+    /**
+        <jp>壁情報をセットアップします
+        <en>
+    */
 	void fixSide(int leftPolyIndex, int rightPolyIndex, int sideIndex,
 		int lineIndex, bool isDeleteOldSide);
 
